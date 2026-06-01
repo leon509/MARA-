51bb7b452b90597091ff9343aecd40d3d8fbe9ee
leon509 <115661644+leon509@users.noreply.github.com>
2026-06-01 04:40:35 +0200

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -18,16 +18,56 @@ class Utility(commands.Cog):
     @commands.command()
     @commands.has_permissions(manage_nicknames=True)
     async def rename(self, ctx, member: discord.Member, *, pseudo):
+
         await member.edit(nick=pseudo)
-        await ctx.send(f"✏️ {member.mention} renommé en **{pseudo}**")
+
+        await ctx.send(
+            f"✏️ {member.mention} renommé en **{pseudo}**"
+        )
 
     @commands.command()
-    async def mp(self, ctx, member: discord.Member, *, message):
+    @commands.has_any_role(
+        "MARA",
+        "MARA C.E.O",
+        "Stagiaires"
+    )
+    async def mp(
+        self,
+        ctx,
+        member: discord.Member,
+        *,
+        message
+    ):
+
         try:
+
             await member.send(message)
-            await ctx.send(f"✅ MP envoyé à {member.mention}")
+
+            await ctx.send(
+                f"✅ MP envoyé à {member.mention}"
+            )
+
         except:
-            await ctx.send("❌ Impossible d'envoyer le message")
+
+            await ctx.send(
+                "❌ Impossible d'envoyer le message"
+            )
+
+    @mp.error
+    async def mp_error(
+        self,
+        ctx,
+        error
+    ):
+
+        if isinstance(
+            error,
+            commands.MissingAnyRole
+        ):
+
+            await ctx.send(
+                "❌ Tu n'as pas la permission d'utiliser cette commande."
+            )
 
 async def setup(bot):
-    await bot.add_cog(Utility(bot)) 
+    await bot.add_cog(Utility(bot))
